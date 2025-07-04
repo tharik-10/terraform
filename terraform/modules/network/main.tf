5ad87ac87a9d84d4d003eb79ce34ab1507f366a3
tharik-10 <md.tharik@mygurukulam.co>
2025-07-04 11:59:33 +0530

    Create main.tf

---
+++ terraform/modules/network/main.tf
@@ -0,0 +1,41 @@
+provider "aws" {
+  region = var.region
+}
+
+resource "aws_vpc" "main" {
+  cidr_block = var.vpc_cidr
+  tags = {
+    Name = "${var.environment}-vpc"
+  }
+}
+
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.main.id
+  tags = {
+    Name = "${var.environment}-igw"
+  }
+}
+
+resource "aws_subnet" "public" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = var.public_subnet_cidr
+  map_public_ip_on_launch = true
+  availability_zone       = var.az
+
+  tags = {
+    Name = "${var.environment}-public-subnet"
+  }
+}
+
+resource "aws_eip" "nat" {
+  vpc = true
+}
+
+resource "aws_nat_gateway" "natgw" {
+  subnet_id     = aws_subnet.public.id
+  allocation_id = aws_eip.nat.id
+
+  tags = {
+    Name = "${var.environment}-natgw"
+  }
+}
