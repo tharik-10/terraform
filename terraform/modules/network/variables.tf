f96d37ce38e03d767a06bd274db2de8f8cda2060
tharik-10 <md.tharik@mygurukulam.co>
2025-07-04 12:00:12 +0530

    Create variables.tf

---
+++ terraform/modules/network/variables.tf
@@ -0,0 +1,15 @@
+variable "region" {
+  type = string
+}
+variable "environment" {
+  type = string
+}
+variable "vpc_cidr" {
+  type = string
+}
+variable "public_subnet_cidr" {
+  type = string
+}
+variable "az" {
+  type = string
+}
