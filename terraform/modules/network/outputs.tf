3f80bb1a9b4c665a9ff3227b3647db5984ddd71c
tharik-10 <md.tharik@mygurukulam.co>
2025-07-05 15:07:00 +0530

    Update outputs.tf

--- terraform/modules/network/outputs.tf
+++ terraform/modules/network/outputs.tf
@@ -1,14 +1,11 @@
 output "vpc_id" {
-  description = "ID of the created VPC"
-  value       = aws_vpc.main.id
+  value = aws_vpc.main.id
 }
 
 output "public_subnet_id" {
-  description = "ID of the public subnet"
-  value       = aws_subnet.public.id
+  value = aws_subnet.public.id
 }
 
 output "igw_id" {
-  description = "ID of the internet gateway"
-  value       = aws_internet_gateway.igw.id
+  value = aws_internet_gateway.igw.id
 }
